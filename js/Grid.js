fcd81f9ceb5f82df5d83a2cac7f8814acb6bc0ee
Harsh-0986 <harsh.r.shah2102@gmail.com>
2023-05-04 21:15:40 +0530

    Implemented the movement of tiles

--- js/Grid.js
+++ js/Grid.js
@@ -17,11 +17,27 @@ export default class Grid {
       );
     });
 
-    console.log(this.cells);
+    console.log(this.#cells);
   }
 
   get #emptyCells() {
     return this.#cells.filter((cell) => cell.tile == null);
+  }
+
+  get cellsByColumn() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.x] = cellGrid[cell.x] || [];
+      cellGrid[cell.x][cell.y] = cell;
+      return cellGrid;
+    }, []);
+  }
+
+  get cellsByRow() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.y] = cellGrid[cell.y] || [];
+      cellGrid[cell.y][cell.x] = cell;
+      return cellGrid;
+    }, []);
   }
 
   randomEmptyCell() {
@@ -35,11 +51,20 @@ class Cell {
   #x;
   #y;
   #tile;
+  #mergeTile;
 
   constructor(cellElement, x, y) {
     this.#cellElement = cellElement;
     this.#x = x;
     this.#y = y;
+  }
+
+  get x() {
+    return this.#x;
+  }
+
+  get y() {
+    return this.#y;
   }
 
   get tile() {
@@ -52,6 +77,24 @@ class Cell {
 
     this.#tile.x = this.#x;
     this.#tile.y = this.#y;
+  }
+
+  get mergeTile() {
+    return this.#mergeTile;
+  }
+
+  set mergeTile(value) {
+    this.#mergeTile = value;
+    if (value == null) return;
+    this.#mergeTile.x = this.#x;
+    this.#mergeTile.y = this.#y;
+  }
+
+  canAccept(tile) {
+    return (
+      this.tile == null ||
+      (this.mergeTile == null && this.tile.value == tile.value)
+    );
   }
 }
 
--- js/script.js
+++ js/script.js
@@ -9,3 +9,71 @@ const grid = new Grid(gameBoard);
 // Adding initial tiles
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
+
+function setupInput() {
+  window.addEventListener("keydown", handleInput, { once: true });
+}
+
+setupInput();
+
+function handleInput(e) {
+  switch (e.key) {
+    case "ArrowUp":
+      moveUp();
+      break;
+    case "ArrowDown":
+      moveDown();
+      break;
+    case "ArrowLeft":
+      moveLeft();
+      break;
+    case "ArrowRight":
+      moveRight();
+      break;
+    default:
+      setupInput();
+      return;
+  }
+
+  setupInput();
+}
+
+function moveUp() {
+  return slideTiles(grid.cellsByColumn);
+}
+
+function moveDown() {
+  return slideTiles(grid.cellsByColumn.map((col) => [...col].reverse()));
+}
+
+function moveLeft() {
+  return slideTiles(grid.cellsByRow);
+}
+
+function moveRight() {
+  return slideTiles(grid.cellsByRow.map((row) => [...row].reverse()));
+}
+
+const slideTiles = (cells) => {
+  cells.forEach((group) => {
+    for (let i = 1; i < group.length; i++) {
+      const cell = group[i];
+      if (cell.tile == null) continue;
+      let lastValidCell;
+      for (let j = i - 1; j >= 0; j--) {
+        const moveToCell = group[j];
+        if (!moveToCell.canAccept(cell.tile)) break;
+        lastValidCell = moveToCell;
+      }
+
+      if (lastValidCell != null) {
+        if (lastValidCell.tile != null) {
+          lastValidCell.mergeTile = cell.tile;
+        } else {
+          lastValidCell.tile = cell.tile;
+        }
+        cell.tile = null;
+      }
+    }
+  });
+};
