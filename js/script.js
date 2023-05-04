d70500b8fc55a9388ead4276132212a3e706d9a5
Harsh-0986 <harsh.r.shah2102@gmail.com>
2023-05-04 18:16:02 +0530

    Created Tile Class and Implemented logic for adding initial tiles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 import Grid from "./Grid.js";
+import Tile from "./Tile.js";
 
-console.log("Console");
+// console.log("Console");
 const gameBoard = document.getElementById("game-board");
 
 const grid = new Grid(gameBoard);
+
+// Adding initial tiles
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
