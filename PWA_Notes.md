934723d8085c443b8c5f521e3afbb98a60e38167
Harsh-0986 <harsh2102.dev@gmail.com>
2023-05-06 12:00:24 +0000

    Publish as PWA

---
+++ PWA_Notes.md
@@ -0,0 +1,3 @@
+# Steps for converting an app to PWA
+
+- Registering service worker to index.html
--- serviceworker.js
+++ serviceworker.js
@@ -1,1 +1,40 @@
-console.log("service");
+const CACHE_NAME = "version-1";
+const URLS_TO_CACHE = ["index.html"];
+const self = this;
+
+// Install Service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+// Listen for requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("index.html"));
+    })
+  );
+});
+
+// Avtivate the service worker
+self.addEventListener("activate", (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhiteList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
